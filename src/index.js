c280547979feee0d5bc7d4c18f4cb3059c76b086
Ivan Rudoy <rudoy@farpost.com>
2019-05-01 00:15:45 +1000

    Add eslint, prettier, editorconfig; lint-staged on pre-commit; Fix codestyle issues

--- src/index.js
+++ src/index.js
@@ -8,25 +8,38 @@ const buildRequire = template(`for (let key in IMPORTED) {
   DIR_IMPORT[key === 'default' ? IMPORTED_NAME : key] = IMPORTED[key]
 }`)
 
-const toCamelCase = (name) =>
+const toCamelCase = name =>
   name.replace(/([-_.]\w)/g, (_, $1) => $1[1].toUpperCase())
 
-const toSnakeCase = (name) =>
-  name.replace(/([-.A-Z])/g, (_, $1) => '_' + ($1 === '.' || $1 === '-' ? '' : $1.toLowerCase()))
+const toSnakeCase = name =>
+  name.replace(
+    /([-.A-Z])/g,
+    (_, $1) => `_${$1 === '.' || $1 === '-' ? '' : $1.toLowerCase()}`,
+  )
 
-const getFiles = (parent, exts = ['js', 'mjs', 'jsx'], nostrip = false, files = [], recursive = false, path = []) => {
-  let r = _fs.readdirSync(parent)
+const getFiles = (
+  parent,
+  exts = ['js', 'mjs', 'jsx'],
+  nostrip = false,
+  files = [],
+  recursive = false,
+  path = [],
+) => {
+  const r = _fs.readdirSync(parent)
 
   for (let i = 0, l = r.length; i < l; i++) {
-    let child = r[i]
+    const child = r[i]
 
-    const {name, ext} = _path.parse(child)
+    const { name, ext } = _path.parse(child)
     const file = path.concat(nostrip ? child : name)
 
     // Check extension is of one of the aboves
     if (exts.includes(ext && ext.length ? ext.slice(1, ext.length) : ext)) {
       files.push(file)
-    } else if (recursive && _fs.statSync(_path.join(parent, child)).isDirectory()) {
+    } else if (
+      recursive &&
+      _fs.statSync(_path.join(parent, child)).isDirectory()
+    ) {
       getFiles(_path.join(parent, name), exts, nostrip, files, recursive, file)
     }
   }
@@ -34,18 +47,20 @@ const getFiles = (parent, exts = ['js', 'mjs', 'jsx'], nostrip = false, files = 
   return files
 }
 
-export default function dir (babel) {
+export default function dir(babel) {
   const { types: t } = babel
 
   return {
     visitor: {
-      ImportDeclaration (path, state) {
-        const {node} = path
-        let src = node.source.value
+      ImportDeclaration(path, state) {
+        const { node } = path
+        const src = node.source.value
 
-        if (src[0] !== '.' && src[0] !== '/') { return }
-        const pathPrefix = src.split('/')[0] + '/';
-        const isAbsolute = src[0] === '/';
+        if (src[0] !== '.' && src[0] !== '/') {
+          return
+        }
+        const pathPrefix = `${src.split('/')[0]}/`
+        const isAbsolute = src[0] === '/'
 
         const isExplicitWildcard = wildcardRegex.test(src)
         let cleanedPath = src.replace(wildcardRegex, '')
@@ -53,109 +68,126 @@ export default function dir (babel) {
         const isRecursive = recursiveRegex.test(cleanedPath)
         cleanedPath = cleanedPath.replace(recursiveRegex, '')
 
-        const sourcePath = this.file.opts.parserOpts.sourceFileName || this.file.opts.parserOpts.filename || ''
+        const sourcePath =
+          this.file.opts.parserOpts.sourceFileName ||
+          this.file.opts.parserOpts.filename ||
+          ''
         const checkPath = _path.resolve(
           isAbsolute
             ? cleanedPath
-            : _path.join(_path.dirname(sourcePath), cleanedPath)
+            : _path.join(_path.dirname(sourcePath), cleanedPath),
         )
 
         try {
           require.resolve(checkPath)
 
           return
-        } catch (e) {}
+        } catch (e) {} // eslint-disable-line no-empty
 
         try {
-          if (!_fs.statSync(checkPath).isDirectory()) { return }
-        } catch (e) { return }
+          if (!_fs.statSync(checkPath).isDirectory()) {
+            return
+          }
+        } catch (e) {
+          return
+        }
 
         const nameTransform = state.opts.snakeCase ? toSnakeCase : toCamelCase
 
-        let _files = getFiles(checkPath, state.opts.exts, state.opts.nostrip, [], isRecursive)
+        // eslint-disable-next-line no-underscore-dangle
+        let _files = getFiles(
+          checkPath,
+          state.opts.exts,
+          state.opts.nostrip,
+          [],
+          isRecursive,
+        )
 
         if (typeof state.opts.listTransform === 'function') {
-          _files = state.opts.listTransform(_files);
+          _files = state.opts.listTransform(_files)
         }
 
-        const files = _files.map((file) =>
-          [file, nameTransform(file[file.length - 1]), path.scope.generateUidIdentifier(file[file.length - 1])]
-        )
+        const files = _files.map(file => [
+          file,
+          nameTransform(file[file.length - 1]),
+          path.scope.generateUidIdentifier(file[file.length - 1]),
+        ])
 
-        if (!files.length) { return }
+        if (!files.length) {
+          return
+        }
 
-        const imports = files.map(([file, fileName, fileUid]) =>
+        const imports = files.map(([file, , fileUid]) =>
           t.importDeclaration(
             node.specifiers.length ? [t.importNamespaceSpecifier(fileUid)] : [],
-            t.stringLiteral((sourcePath || isAbsolute ? '' : pathPrefix) + _path.join(cleanedPath, ...file))
-          )
+            t.stringLiteral(
+              (sourcePath || isAbsolute ? '' : pathPrefix) +
+                _path.join(cleanedPath, ...file),
+            ),
+          ),
         )
 
-        let dirVar = path.scope.generateUidIdentifier('dirImport')
+        const dirVar = path.scope.generateUidIdentifier('dirImport')
         if (node.specifiers.length) {
-          path.insertBefore(t.variableDeclaration(
-            'const', [
-              t.variableDeclarator(dirVar, t.objectExpression([]))
-            ]
-          ))
+          path.insertBefore(
+            t.variableDeclaration('const', [
+              t.variableDeclarator(dirVar, t.objectExpression([])),
+            ]),
+          )
         }
 
         for (let i = node.specifiers.length - 1; i >= 0; i--) {
-          let dec = node.specifiers[i]
+          const dec = node.specifiers[i]
 
-          if (t.isImportNamespaceSpecifier(dec) || t.isImportDefaultSpecifier(dec)) {
-            path.insertAfter(t.variableDeclaration(
-              'const', [
-                t.variableDeclarator(
-                  t.identifier(dec.local.name),
-                  dirVar
-                )
-              ]
-            ))
+          if (
+            t.isImportNamespaceSpecifier(dec) ||
+            t.isImportDefaultSpecifier(dec)
+          ) {
+            path.insertAfter(
+              t.variableDeclaration('const', [
+                t.variableDeclarator(t.identifier(dec.local.name), dirVar),
+              ]),
+            )
           }
 
           if (t.isImportSpecifier(dec)) {
-            path.insertAfter(t.variableDeclaration(
-              'const', [
+            path.insertAfter(
+              t.variableDeclaration('const', [
                 t.variableDeclarator(
                   t.identifier(dec.local.name),
-                  t.memberExpression(
-                    dirVar,
-                    t.identifier(dec.imported.name)
-                  )
-                )
-              ]
-            ))
+                  t.memberExpression(dirVar, t.identifier(dec.imported.name)),
+                ),
+              ]),
+            )
           }
         }
 
         if (node.specifiers.length) {
           if (isExplicitWildcard) {
-            files.forEach(([file, fileName, fileUid]) =>
-              path.insertAfter(buildRequire({
-                IMPORTED_NAME: t.stringLiteral(fileName),
-                DIR_IMPORT: dirVar,
-                IMPORTED: fileUid
-              }))
+            files.forEach(([, fileName, fileUid]) =>
+              path.insertAfter(
+                buildRequire({
+                  IMPORTED_NAME: t.stringLiteral(fileName),
+                  DIR_IMPORT: dirVar,
+                  IMPORTED: fileUid,
+                }),
+              ),
             )
           } else {
-            files.forEach(([file, fileName, fileUid]) =>
+            files.forEach(([, fileName, fileUid]) =>
               path.insertAfter(
                 t.assignmentExpression(
                   '=',
-                  t.memberExpression(
-                    dirVar,
-                    t.identifier(fileName)
-                  ),
-                  fileUid
-                )
-              )
+                  t.memberExpression(dirVar, t.identifier(fileName)),
+                  fileUid,
+                ),
+              ),
             )
           }
         }
 
         path.replaceWithMultiple(imports)
-      }
-    }
+      },
+    },
   }
 }
--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,26 +1,27 @@
-import '@babel/register';
-import * as babel from '@babel/core';
-import path from 'path';
-import plugin from '../index.js';
+import '@babel/register'
+import * as babel from '@babel/core'
+import path from 'path'
+import plugin from '../index'
 
-const transform = (code, opts = {}) => babel.transform(code, { babelrc: false, plugins: [[plugin, opts]] }).code;
+const transform = (code, opts = {}) =>
+  babel.transform(code, { babelrc: false, plugins: [[plugin, opts]] }).code
 
 test('importing without specifiers', () => {
-  const orig = `import './src/test/fixtures/**';`;
+  const orig = `import './src/test/fixtures/**';`
 
   expect(transform(orig)).toEqual(`import "./src/test/fixtures/c/fakeModuleD";
 import "./src/test/fixtures/fake-module-b";
-import "./src/test/fixtures/fake.module.a";`);
-});
+import "./src/test/fixtures/fake.module.a";`)
+})
 
-test('importing a module shouldn\'t do nothing', () => {
-  const orig = `import x from 'fake-module';`;
+test("importing a module shouldn't do nothing", () => {
+  const orig = `import x from 'fake-module';`
 
-  expect(transform(orig)).toEqual(orig);
-});
+  expect(transform(orig)).toEqual(orig)
+})
 
 test('importing the absolute path', () => {
-  const resolvedPath = path.resolve('./src/test/fixtures');
+  const resolvedPath = path.resolve('./src/test/fixtures')
   const orig = `import '${resolvedPath}/**';`
 
   expect(transform(orig)).toEqual(`import "${resolvedPath}/c/fakeModuleD";
@@ -28,68 +29,69 @@ import "${resolvedPath}/fake-module-b";
 import "${resolvedPath}/fake.module.a";`)
 })
 
-test('importing a existing file shouldn\'t do nothing', () => {
-  const orig = `import x from './src/index.js';`;
+test("importing a existing file shouldn't do nothing", () => {
+  const orig = `import x from './src/index.js';`
 
-  expect(transform(orig)).toEqual(orig);
-});
+  expect(transform(orig)).toEqual(orig)
+})
 
-test('importing a existing directory with an index file shouldn\'t do nothing', () => {
+test("importing a existing directory with an index file shouldn't do nothing", () => {
   const orig = `import x from './src';`
 
-  expect(transform(orig)).toEqual(orig);
-});
+  expect(transform(orig)).toEqual(orig)
+})
 
-test('importing a existing directory without javascript files shouldn\'t do nothing', () => {
-  const orig = `import x from './src/test/fixtures/d';`;
+test("importing a existing directory without javascript files shouldn't do nothing", () => {
+  const orig = `import x from './src/test/fixtures/d';`
 
-  expect(transform(orig)).toEqual(orig);
-});
+  expect(transform(orig)).toEqual(orig)
+})
 
 test('importing a existing directory without index file', () => {
-  const orig = `import x from './src/test/fixtures';`;
+  const orig = `import x from './src/test/fixtures';`
 
   expect(transform(orig)).toEqual(`const _dirImport = {};
 import * as _fakeModuleB from "./src/test/fixtures/fake-module-b";
 import * as _fakeModuleA from "./src/test/fixtures/fake.module.a";
 _dirImport.fakeModuleA = _fakeModuleA
 _dirImport.fakeModuleB = _fakeModuleB
-const x = _dirImport;`);
-});
+const x = _dirImport;`)
+})
 
 test('importing a existing directory without index file nostrip', () => {
-  const orig = `import x from './src/test/fixtures';`;
+  const orig = `import x from './src/test/fixtures';`
 
   expect(transform(orig, { nostrip: true })).toEqual(`const _dirImport = {};
 import * as _fakeModuleBJs from "./src/test/fixtures/fake-module-b.js";
 import * as _fakeModuleAJs from "./src/test/fixtures/fake.module.a.js";
 _dirImport.fakeModuleAJs = _fakeModuleAJs
 _dirImport.fakeModuleBJs = _fakeModuleBJs
-const x = _dirImport;`);
-});
+const x = _dirImport;`)
+})
 
 test('importing a existing directory without index file listTransform, sort by depth', () => {
-  const orig = `import './src/test/fixtures/**';`;
-  const listTransform = (list) => list.sort((a, b) => (a.length - b.length));
+  const orig = `import './src/test/fixtures/**';`
+  const listTransform = list => list.sort((a, b) => a.length - b.length)
 
-  expect(transform(orig, { listTransform })).toEqual(`import "./src/test/fixtures/fake-module-b";
+  expect(transform(orig, { listTransform }))
+    .toEqual(`import "./src/test/fixtures/fake-module-b";
 import "./src/test/fixtures/fake.module.a";
-import "./src/test/fixtures/c/fakeModuleD";`);
-});
+import "./src/test/fixtures/c/fakeModuleD";`)
+})
 
 test('importing a existing directory without index file', () => {
-  const orig = `import x from './src/test/fixtures';`;
+  const orig = `import x from './src/test/fixtures';`
 
   expect(transform(orig, { snakeCase: true })).toEqual(`const _dirImport = {};
 import * as _fakeModuleB from "./src/test/fixtures/fake-module-b";
 import * as _fakeModuleA from "./src/test/fixtures/fake.module.a";
 _dirImport.fake_module_a = _fakeModuleA
 _dirImport.fake_module_b = _fakeModuleB
-const x = _dirImport;`);
-});
+const x = _dirImport;`)
+})
 
 test('importing a existing directory without index file', () => {
-  const orig = `import x from './src/test/fixtures/*';`;
+  const orig = `import x from './src/test/fixtures/*';`
 
   expect(transform(orig)).toEqual(`const _dirImport = {};
 import * as _fakeModuleB from "./src/test/fixtures/fake-module-b";
@@ -103,11 +105,11 @@ for (let key in _fakeModuleB) {
   _dirImport[key === 'default' ? "fakeModuleB" : key] = _fakeModuleB[key];
 }
 
-const x = _dirImport;`);
-});
+const x = _dirImport;`)
+})
 
 test('importing a existing directory without index file', () => {
-  const orig = `import x from './src/test/fixtures/**';`;
+  const orig = `import x from './src/test/fixtures/**';`
 
   expect(transform(orig)).toEqual(`const _dirImport = {};
 import * as _fakeModuleD from "./src/test/fixtures/c/fakeModuleD";
@@ -116,11 +118,11 @@ import * as _fakeModuleA from "./src/test/fixtures/fake.module.a";
 _dirImport.fakeModuleA = _fakeModuleA
 _dirImport.fakeModuleB = _fakeModuleB
 _dirImport.fakeModuleD = _fakeModuleD
-const x = _dirImport;`);
-});
+const x = _dirImport;`)
+})
 
 test('importing a existing directory without index file', () => {
-  const orig = `import x from './src/test/fixtures/**/*';`;
+  const orig = `import x from './src/test/fixtures/**/*';`
 
   expect(transform(orig)).toEqual(`const _dirImport = {};
 import * as _fakeModuleD from "./src/test/fixtures/c/fakeModuleD";
@@ -139,11 +141,11 @@ for (let key in _fakeModuleD) {
   _dirImport[key === 'default' ? "fakeModuleD" : key] = _fakeModuleD[key];
 }
 
-const x = _dirImport;`);
-});
+const x = _dirImport;`)
+})
 
 test('importing a existing directory without index file', () => {
-  const orig = `import { fakeModuleA } from './src/test/fixtures/*';`;
+  const orig = `import { fakeModuleA } from './src/test/fixtures/*';`
 
   expect(transform(orig)).toEqual(`const _dirImport = {};
 import * as _fakeModuleB from "./src/test/fixtures/fake-module-b";
@@ -157,11 +159,11 @@ for (let key in _fakeModuleB) {
   _dirImport[key === 'default' ? "fakeModuleB" : key] = _fakeModuleB[key];
 }
 
-const fakeModuleA = _dirImport.fakeModuleA;`);
-});
+const fakeModuleA = _dirImport.fakeModuleA;`)
+})
 
 test('importing a existing directory without index file', () => {
-  const orig = `import { fakeModuleA as methodA, someMethodThatReallyDontExists } from './src/test/fixtures/*';`;
+  const orig = `import { fakeModuleA as methodA, someMethodThatReallyDontExists } from './src/test/fixtures/*';`
 
   expect(transform(orig)).toEqual(`const _dirImport = {};
 import * as _fakeModuleB from "./src/test/fixtures/fake-module-b";
@@ -177,4 +179,4 @@ for (let key in _fakeModuleB) {
 
 const methodA = _dirImport.fakeModuleA;
 const someMethodThatReallyDontExists = _dirImport.someMethodThatReallyDontExists;`)
-});
+})
